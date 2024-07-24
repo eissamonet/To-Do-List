46cd019ff125291f478132243f8c97366520b3f8
Eissa Purnell <eissamonet@gmail.com>
2024-07-23 20:15:11 -0500

    input field and title added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 import todo_icon from '../assets/todo_icon.png'
+import TodoItems from './TodoItems'
 
 const Todo = () => {
   return (
-    <div className='bg-white place-self-center w-11/12 max-w-md flex lex-colum p-7 min-h-[550px] rounded-xl'>
+    <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[750px] rounded-2xl'>
 
-{/* ----------- Title -------- */}
+
+{/*------------title----------------*/}
+    <div className='flex items-center mt-7 gap-2'>
+      <img className='w-6' src={todo_icon} alt="" />
+      <h1 className='text-2xl font-semibold'>To-Do List</h1>
+    </div>
+
+{/*------------input----------------*/}
+    <div className='flex items-center my-7 bg-gray-100 rounded-full'>
+        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add a new task'/>
+        <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-l font-medium cursor-pointer'>ADD + </button>
+    </div>
 
     <div>
-      <img className='w-8' src={todo_icon} alt="" />
-      <h1 className='text-3xl font-semibold'>To-Do List</h1>
+      <TodoItems/>
     </div>
-
-{/* ----------- Input -------- */}
-
-    <div className='flex items my-7 bg-gray-200 rounded-full'>
-      <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Add your task here'/>
-      <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Add +</button>
-    </div>
-
-{/* ----------- Tasks -------- */}
-
-
 
     </div>
   )
---
+++ src/components/TodoItems.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import tick from '../assets/tick.png'
+import not_tick from '../assets/not_tick.png'
+import delete_icon from '../assets/delete.png'
+
+const TodoItems = () => {
+  return (
+    <div className='flex items-center my-3 gap-2'>
+      <div className='flex-1 items-center cursor-pointer'>
+      </div>
+
+
+    </div>
+  )
+}
+
+export default TodoItems
